0630879c6ec6a1ccb0cdbc311e2b4dcacdddf5a9
Ítalo Rodrigo <italo.computation@gmail.com>
2022-07-25 19:58:35 -0400

    Integração com Telegram bem-sucedida
    Integração com Telegram bem-sucedida

--- bot_blaze/app/Http/Controllers/Teste/TesteController.php
+++ bot_blaze/app/Http/Controllers/Teste/TesteController.php
@@ -4,8 +4,33 @@ namespace App\Http\Controllers\Teste;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Telegram\Bot\Api;
 
 class TesteController extends Controller
 {
-    //
+
+    public function teste(){
+        $telegram = new Api('5546066078:AAGu7OdrsRjiGQwl_IY2kBIgNnqy_7OSKCU');
+        // $telegram = new Api('TELEGRAM_BOT_TOKEN');
+        $teste = $telegram->getUpdates();
+
+        // acessos
+        /**
+         * 1 - $teste[0]['update_id']
+         * 2 -
+         */
+
+        dd($teste[0]['message']);
+
+
+        //140321351
+        // $response = $telegram->sendMessage([
+        //     'chat_id' => '-1001546814958',
+        //     'text' => 'Hello World!! Oi amor deu certo, de novo!!!!'
+        // ]);
+        // $messageId = $response->getMessageId();
+        // dd($teste);
+
+        return $teste;
+    }
 }
--- bot_blaze/config/telegram.php
+++ bot_blaze/config/telegram.php
@@ -34,7 +34,7 @@ return [
     'bots'                         => [
         'mybot' => [
             'username'            => 'TelegramBot',
-            'token'               => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
+            'token'               => env('TELEGRAM_BOT_TOKEN', '5546066078:AAGu7OdrsRjiGQwl_IY2kBIgNnqy_7OSKCU'),
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands'            => [
