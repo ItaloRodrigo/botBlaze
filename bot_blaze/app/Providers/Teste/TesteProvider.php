c24ecf92159b951216fae6d9e2cf527b9304b90f
Ítalo Rodrigo <italo.computation@gmail.com>
2022-07-25 17:12:50 -0400

    Teste de Controller e Provider
    Teste de Controller e Provider

---
+++ bot_blaze/app/Providers/Teste/TesteProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers\Teste;
+
+use Illuminate\Support\ServiceProvider;
+
+class TesteProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
